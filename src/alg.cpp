4e2b5fa9c1147ea28f15f38408d0d1d84e845068
fedek1324 <62599359+fedek1324@users.noreply.github.com>
2020-04-10 16:28:35 +0300

    Create alg.cpp

---
+++ src/alg.cpp
@@ -0,0 +1,75 @@
+int countPairs1(int *arr,int len,int value)
+{
+    int counter = 0;
+    for (int i = 0; i < len - 1; i++)
+        for(int j = i+1; j < len; j++)
+            if (arr[i] + arr[j] == value)
+                counter++;
+    return counter;
+}
+
+int countPairs2(int* arr, int len, int value)
+{
+	int k = 0;
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = len - 1; j > i; j--)
+		{
+			if (arr[i] + arr[j] == value)
+			{
+				k++;
+
+			}
+            if (arr[i] + arr[j] < value)
+                break;
+		}
+	}
+	return k;
+}
+
+int findRightBoundIndexInBounds(int *arr, int n, int element, int leftBound, int rightBound)
+{
+    int midIndex = -1;
+    while(true)
+    {
+        if (rightBound == leftBound + 1) return rightBound;
+        midIndex = (leftBound + rightBound)/2;
+        if (arr[midIndex] <= element)
+            leftBound = midIndex;
+        else 
+            rightBound = midIndex;
+    }
+}
+
+int findLeftBoundIndexInBounds(int *arr, int len, int element, int leftBound, int rightBound)
+{
+    int midIndex = -1;
+    while (true)
+    {
+        if (rightBound == leftBound + 1)
+            return leftBound;
+        midIndex = (leftBound + rightBound) / 2;
+        if (arr[midIndex] < element)
+            leftBound = midIndex;
+        else
+            rightBound = midIndex;
+    }
+}
+
+int countPairs3(int *arr,int len,int value)
+{
+    int counter = 0;
+    int leftBound = 0;
+    int rightBound = 0;
+    int elementForPair = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        elementForPair = value - arr[i];
+        if (elementForPair < arr[i])
+            return counter;
+        leftBound = findLeftBoundIndexInBounds(arr, len, elementForPair, i, len);
+        rightBound = findRightBoundIndexInBounds(arr, len, elementForPair, i, len);
+        counter += rightBound - leftBound - 1;
+    }
+    return counter;
+}
