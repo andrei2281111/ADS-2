c304eaf3d2533d4019907ea77fc86d85a21e9913
EMara00 <62856938+EMara00@users.noreply.github.com>
2020-04-18 22:52:20 +0300

    Update alg.cpp

--- alg.cpp
+++ alg.cpp
@@ -1,67 +1,65 @@
-int countPairs1(int* arr, int len, int value)
+int countPairs1(int *arr, int len, int value)
 {
-	int k = 0;
-	for (int i = 0; i < len-1; i++)
-	{
-		for (int j = i + 1; j < len; j++)
-		{
-			if (arr[i] + arr[j] == value)
-				k++;
-		}
-	}
-
-	return k;
+    int result = 0;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = i; j < len; j++) 
+        {
+            if (arr[i] + arr[j] == value) 
+            {
+                result++;
+            }
+        }
+    }
+    return result; 
 }
-int countPairs2(int* arr, int len, int value)
+
+int countPairs2(int *arr, int len, int value)
 {
-	int k = 0;
-	for (int i = 0; i < len - 1; i++)
-	{
-		for (int j = len - 1; j > i; j--)
-		{
-			if (arr[i] + arr[j] == value)
-			{
-				k++;
-
-			}
-		}
-	}
-	return k;
+    int result = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = len - 1; i < j; j--) 
+        {
+            if (arr[i] + arr[j] == value) 
+            {
+                result++;
+            }
+        }
+    }
+    return result;
 }
-int countPairs3(int* arr, int len, int value)
-{int k = 0;
-	for (int i = 0; i < len - 1; i++)
-	{
-		int left = i, right = len;
 
-		while (left < right-1)
-		{
-			int mid = (left + right) / 2;
-			if (arr[i] + arr[mid] == value)
-			{
-				k++;
-				int j = mid+1;
-				while (arr[i] + arr[j] == value && j <right)
-				{
-					k++;
-					j++;
-				}
-				j = mid - 1;
-				while (arr[i] + arr[j] == value && j>left)
-				{
-					k++;
-					j--;
-				}
-				break;
-			}
-			if (arr[i] + arr[mid] > value)
-				right = mid;
-			else
-				left = mid;
-		}
-
-	}
-
-
-	return k;
+int countPairs3(int *arr, int len, int value)
+{
+    int result = 0;
+    for (int i = 0; i < len; i++)
+    {
+        int search = value - arr[i];
+        int middle = ((len - i)/2) + i;
+        for (int start = i, end = len; middle != start || middle != end; middle = ((end - start) / 2) + start)
+        {
+            if (arr[middle] == search)
+            {
+                while (arr[middle] == search) 
+                {
+                    middle--; 
+                }
+                while (arr[++middle] == search) 
+                { 
+                    result++;
+                }
+                break;
+            }
+            if (arr[middle] < search)
+            {
+                start = middle;
+            }
+            else
+            {
+                end = middle;
+            }
+        }
+    }
+    return result;
 }
