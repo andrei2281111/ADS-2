79109cf682ca061db6a10949c0a412b0752fb98e
EMara00 <62856938+EMara00@users.noreply.github.com>
2020-04-19 00:17:46 +0300

    Create alg.cpp

---
+++ src/alg.cpp
@@ -0,0 +1,60 @@
+int countPairs1(int *arr, int len, int value)
+{
+    int result = 0;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = i; j < len; j++) 
+        {
+            if (arr[i] + arr[j] == value) 
+            {
+                result++;
+            }
+        }
+    }
+    return result; //4
+}
+
+int countPairs2(int *arr, int len, int value)
+{
+    int result = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = len - 1; i < j; j--) 
+        {
+            if (arr[i] + arr[j] == value) 
+            {
+                result++;
+            }
+        }
+    }
+    return result;
+}
+
+int countPairs3(int *arr, int len, int value)
+{
+    int result = 0;
+    for (int i = 0; i < len; i++)
+    {
+        int search = value - arr[i];
+        int middle = ((len - i)/2) + i;
+        for (int start = i, end = len; middle != start || middle != end; middle = ((end - start) / 2) + start)
+        {
+            //std::cout << start << " " << search << " " << end << "\n";
+            if (arr[middle] == search)
+            {
+                for (; arr[middle] == search; middle--) { }
+                for (; arr[++middle] == search; result++) { }
+                break;
+            }
+            if (arr[middle] < search)
+            {
+                start = middle;
+            }
+            else
+            {
+                end = middle;
+            }
+        }
+    }
+    return result;
+}
