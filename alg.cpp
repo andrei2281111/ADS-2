eb2ce3b02c2c79e8debfdd49aa8b31badbf3d61b
EMara00 <62856938+EMara00@users.noreply.github.com>
2020-04-18 22:48:55 +0300

    Update alg.cpp

--- alg.cpp
+++ alg.cpp
@@ -1,61 +1,67 @@
-int countPairs1(int *arr, int len, int value)
+int countPairs1(int* arr, int len, int value)
 {
-    int result = 0;
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = i; j < len; j++) 
-        {
-            if (arr[i] + arr[j] == value) 
-            {
-                result++;
-            }
-        }
-    }
-    return result; 
-}
+	int k = 0;
+	for (int i = 0; i < len-1; i++)
+	{
+		for (int j = i + 1; j < len; j++)
+		{
+			if (arr[i] + arr[j] == value)
+				k++;
+		}
+	}
 
-int countPairs2(int *arr, int len, int value)
-{
-    int result = 0;
-    for (int i = 0; i < len - 1; i++)
-    {
-        for (int j = len - 1; i < j; j--) 
-        {
-            if (arr[i] + arr[j] == value) 
-            {
-                result++;
-            }
-        }
-    }
-    return result;
+	return k;
 }
-
-int countPairs3(int *arr, int len, int value)
+int countPairs2(int* arr, int len, int value)
 {
-	int n = 0;
-	for (int i = 0; i < len - 1; i++){
-		int first = i;
-		int second = len;
-		while (first < second - 1){
-			int mid = (first + second) / 2;
-			if (arr[i] + arr[mid] == value){
-				    n++;
-				    int j = mid + 1;
-				while (arr[i] + arr[j] == value && j < second){
-					n++;
+	int k = 0;
+	for (int i = 0; i < len - 1; i++)
+	{
+		for (int j = len - 1; j > i; j--)
+		{
+			if (arr[i] + arr[j] == value)
+			{
+				k++;
+
+			}
+		}
+	}
+	return k;
+}
+int countPairs3(int* arr, int len, int value)
+{int k = 0;
+	for (int i = 0; i < len - 1; i++)
+	{
+		int left = i, right = len;
+
+		while (left < right-1)
+		{
+			int mid = (left + right) / 2;
+			if (arr[i] + arr[mid] == value)
+			{
+				k++;
+				int j = mid+1;
+				while (arr[i] + arr[j] == value && j <right)
+				{
+					k++;
 					j++;
 				}
-			j = mid - 1;
-			while (arr[i] + arr[j] == value && j> first){
-				n++;
-				j--;
+				j = mid - 1;
+				while (arr[i] + arr[j] == value && j>left)
+				{
+					k++;
+					j--;
+				}
+				break;
 			}
-			break;
+			if (arr[i] + arr[mid] > value)
+				right = mid;
+			else
+				left = mid;
 		}
-		if (arr[i] + arr[mid] > value) 
-		second = mid;
-		else first = mid;
+
 	}
-}
-	return n;
+
+
+	return k;
 }
