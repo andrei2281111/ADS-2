bff10391e8572a93408418981a85538af968505a
EMara00 <62856938+EMara00@users.noreply.github.com>
2020-04-18 22:34:34 +0300

    Update alg.cpp

--- alg.cpp
+++ alg.cpp
@@ -30,31 +30,46 @@ int countPairs2(int *arr, int len, int value)
     return result;
 }
 
-int countPairs3(int* arr, int len, int value) {
-    int result = 0;
-    for (int i = 0; i < len - 1; i++) {
-        int left = i, right = len;
-        while (left < right - 1) {
-            int mid = (left + right) / 2;
-            if (arr[i] + arr[mid] == value) {
-                result++;
-                int j = mid + 1;
-                while (arr[i] + arr[j] == value && j < right) {
-                    result++;
-                    j++;
-                }
-                j = mid - 1;
-                while (arr[i] + arr[j] == value && j > left) {
-                    result++;
-                    j--;
-                }
-                break;
-            }
-            if (arr[i] + arr[mid] > value)
-                right = mid;
-            else
-                left = mid;
-        }
-    }
-    return result;
+int countPairs3(int* arr, int len, int value)
+{
+	int count = 0;
+	int l;
+	int r;
+	int mid;
+
+	for (int i = 0; i < len; i++)
+	{
+		l = i + 1;
+		r = len - 1;
+		mid = (r - l) / 2;
+		while (l<=r)
+		{
+			mid = (r + l) / 2;
+			if (arr[i] + arr[mid] == value)
+			{
+				count++;
+				int k = mid + 1, k_1 = mid - 1;
+				while (arr[i] + arr[k]==value&&k<=r)
+				{
+					count++;
+					k++;
+				}
+				while (arr[i] + arr[k_1]==value&&k_1>i)
+				{
+					count++;
+					k_1--;
+				}
+				break;
+			}
+			else if (arr[i] + arr[mid] < value)
+			{
+				l = mid+1;
+			}
+			else
+			{
+				r = mid-1;
+			}
+		}
+	}
+	return count;
 }
