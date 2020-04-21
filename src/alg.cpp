fa825b09b54e4cb2c3cd8276735c3c8222bb340d
KseniaStorozheva <62885086+KseniaStorozheva@users.noreply.github.com>
2020-04-21 22:42:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,58 +1,52 @@
 int countPairs1(int* arr, int len, int value)
 {
 	int k = 0;
-	for (int i = 0; i <= len; i++)
-		for (int j = i + 1; j <= len; j++)
-			if (arr[i] + arr[j] == value)
-				k++;
-
-	return k;
-}
-
-int countPairs2(int* arr, int len, int value)
-{
-	int k = 0;
-	while (arr[len - 1] > value)
-		len--;
-	int j;
-	int i = 0;
-	while (arr[i] <= value / 2)
+	for (int i = 0; i < len-1; i++)
 	{
-		j = len - 1;
-		while ((arr[i] + arr[j] >= value) && (j > i))
+		for (int j = i + 1; j < len; j++)
 		{
 			if (arr[i] + arr[j] == value)
 				k++;
-			j--;
 		}
-		i++;
 	}
-
 	return k;
 }
-
-
-int countPairs3(int* arr, int len, int value)
+int countPairs2(int* arr, int len, int value)
 {
 	int k = 0;
 	for (int i = 0; i < len - 1; i++)
 	{
+		for (int j = len - 1; j > i; j--)
+		{
+			if (arr[i] + arr[j] == value)
+			{
+				k++;
+
+			}
+		}
+	}
+	return k;
+}
+int countPairs3(int* arr, int len, int value)
+{int k = 0;
+	for (int i = 0; i < len - 1; i++)
+	{
 		int left = i, right = len;
 
-		while (left < right - 1)
+		while (left < right-1)
 		{
 			int mid = (left + right) / 2;
 			if (arr[i] + arr[mid] == value)
 			{
 				k++;
-				int j = mid + 1;
-				while ((arr[i] + arr[j] == value) && (j < right))
+				int j = mid+1;
+				while (arr[i] + arr[j] == value && j <right)
 				{
 					k++;
 					j++;
 				}
 				j = mid - 1;
-				while ((arr[i] + arr[j] == value) && (j > left))
+				while (arr[i] + arr[j] == value && j>left)
 				{
 					k++;
 					j--;
@@ -64,7 +58,6 @@ int countPairs3(int* arr, int len, int value)
 			else
 				left = mid;
 		}
-
 	}
 	return k;
 }
