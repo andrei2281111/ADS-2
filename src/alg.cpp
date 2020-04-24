fd725282bfc33c15be562a72a74040aa4199377b
AnnGera <62617137+AnnGera@users.noreply.github.com>
2020-04-24 21:34:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,65 @@
-int countPairs1(int* arr, int len, int value) { 
-int search = 0; 
-for (int i = 0; i < len - 1; i++) { 
-for (int j = i + 1; j < len; j++) { 
-if ((arr[i] + arr[j]) == value) { 
-search = search+1; 
-} 
-} 
-} 
-return search; 
-} 
-int countPairs2(int* arr, int len, int value) { 
-int search = 0; 
-for (int i = 0; i < len - 1; i++) 
-{ 
-for (int j = len - 1; j > i; j--) 
-{ 
-if (arr[i] + arr[j] == value) 
-{ 
-search = search + 1; 
-} 
-} 
-} 
-return search; 
-} 
-int countPairs3(int* arr, int len, int value) { 
-int search = 0; 
-for (int i = 0; i < len; i++) 
-{ 
-int first = 0; 
-int last = len - 1; 
-while (first <= last) { 
-int mid = first + (last - first) / 2; 
-if (arr[mid] + arr[i] == value&&mid>i) { 
-search = search + 1; 
-int k = 1; 
-while ((arr[mid + k] + arr[i]) == value && ((mid + k) <= last)) { 
-search = search + 1; 
-k++; 
-} 
-k = 1; 
-while ((arr[mid - k] + arr[i]) == value && ((mid - k) > i)) { 
-search = search + 1; 
-k++; 
-} 
-break; 
-} 
-if (arr[mid] + arr[i] < value) 
-first = mid + 1; 
-else last = mid - 1; 
-} 
-} 
-return search; 
-} 
+int countPairs1(int* arr, int len, int value) {
+    int search = 0;
+    for (int i = 0; i<len-1; i++) {
+        for (int j = i+1; j<len; j++) {
+            if (arr[i]+arr[j]==value) {
+                search++;
+            }
+        }
+    }
+    return search;
+}
+int countPairs2(int* arr, int len, int value) {
+    int search = 0;
+    int i = 0;
+    int k;
+    int j = len-1;
+    while (true) {
+        if (i == j)
+            return search;
+        else if (arr[i]+arr[j] <value)
+            i++;
+        else if (arr[i]+arr[j] >value)
+            j--;
+        else  if (arr[i]+arr[j]==value) {
+            search++;
+            for (k = i+1; k<j; k++) {
+                if (arr[k]+arr[j]==value)
+                    search++;
+            }
+            j = j - 1;
+        }
+    }
+}
+int countPairs3(int* arr, int len, int value)
+{int search = 0;
+    for (int i = 0; i<len-1; i++)
+    {
+        int first = i;
+        int last = len;
+        while (first<last-1){
+            int mid = (first+last)/2;
+            if (arr[mid]+arr[i]==value)
+            {
+                search++;
+                int k = mid+1;
+                while (arr[k]+arr[i]==value && k<last)
+                {
+                    search++;
+                    k++;
+                }
+                k = mid-1;
+                while (arr[k]+arr[i]==value && k>first)
+                {
+                    search++;
+                    k--;
+                }
+                break;
+            }
+            if (arr[mid]+arr[i]>value)
+                last = mid;
+            else first = mid;
+        }
+    }
+    return search;
+}
