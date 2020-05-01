bf002cf7b6f960542429a80b2a364d3aa4e92d2a
DmtryP <62616017+DmtryP@users.noreply.github.com>
2020-05-01 17:11:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,69 +1,76 @@
-int countPairs1(int *arr,int len,int value)
-
+int countPairs1(int* arr, int len, int value)
 {
-    
-    
-    int schetchik=0;
+	if ((len == 0) || (len == 1) || (arr[len - 1] < value / 2))
+		return 0;
+	int k = 0;
+	for (int i = 0; i <= len; i++)
+		for (int j = i + 1; j <= len; j++)
+			if (arr[i] + arr[j] == value)
+				k++;
 
-	for (int i = 0; i < len; i++){
-
-        	for (int j = 0; j < len; j++){
-
-            		if (arr[i] + arr[j] == value){
-
-            		schetchik++;
-
-            		}
-    
-    	}
-
-    	}
-
-	return schetchik;
+	return k;
 }
 
-int countPairs2(int *arr,int len,int value)
+int countPairs2(int* arr, int len, int value)
 {
-
-
-	int schetchik=0;
-	while (*arr < len-1) {
-		int s = arr[*arr] + arr[len-1];
-		if (s == value) {
-			schetchik++;
-			*arr++;
-			len--;
-		} else {
-			if (s < value)
-			*arr++;
-			else len--;
+	if ((len == 0) || (len == 1) || (arr[len - 1] < value / 2))
+		return 0;
+	int k = 0;
+	while (arr[len - 1] > value)
+		len--;
+	int j;
+	int i = 0;
+	while (arr[i] <= value / 2)
+	{
+		j = len - 1;
+		while ((arr[i] + arr[j] >= value) && (j > i))
+		{
+			if (arr[i] + arr[j] == value)
+				k++;
+			j--;
 		}
+		i++;
 	}
-	return schetchik;
+
+	return k;
 }
 
 
-int countPairs3(int *arr,int len,int value)
+int countPairs3(int* arr, int len, int value)
 {
+	if ((len == 0) || (len == 1) || (arr[len - 1] < value / 2))
+		return 0;
+	int k = 0;
+	for (int i = 0; i < len - 1; i++)
+	{
+		int left = i, right = len;
 
+		while (left < right - 1)
+		{
+			int mid = (left + right) / 2;
+			if (arr[i] + arr[mid] == value)
+			{
+				k++;
+				int j = mid + 1;
+				while ((arr[i] + arr[j] == value) && (j < right))
+				{
+					k++;
+					j++;
+				}
+				j = mid - 1;
+				while ((arr[i] + arr[j] == value) && (j > left))
+				{
+					k++;
+					j--;
+				}
+				break;
+			}
+			if (arr[i] + arr[mid] > value)
+				right = mid;
+			else
+				left = mid;
+		}
 
-	int schetchik=0;
-
-	for (int i = 0; i < array.Length; i++){
-		
-		int mid = *arr + (len - *arr) / 2;
-	
-		if (array[i] + array[mid] == value){
-		schetchik=+1;
-        	return countPairs3(mid + 1, len, value);
-		}
-	
-		else if (array[mid] > value){
-        	return countPairs3(*arr, mid, value);
-		}
-    		else{
-       		return countPairs3(mid + 1, len, value);
-		}
 	}
-	return schetchik;
+	return k;
 }
