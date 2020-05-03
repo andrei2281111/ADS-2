bfbb0b07222fbd938101ff9e039ce57db87f670e
ksidyagin <64638744+ksidyagin@users.noreply.github.com>
2020-05-03 22:45:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,10 @@
-int cbinsearch(int* arr, int size, int value)
-{
-	int left = 0;
-	int right = size - 1;
-
-	while (left <= right) {
-		int mid = (left + right) / 2;
-		if (arr[mid] == value) {
-			
-			return mid;
-		}
-
-		else if (arr[mid] < value)
-			left = mid + 1;
-		else if (arr[mid] > value)
-			right = mid - 1;
-	}
-	return -1;
-}
-
-
 int countPairs1(int* arr, int len, int value)
 {
 	int count = 0;
 	for (int i = 0; i < len; i++) {
 		for (int j = len - 1; j > 0; j--)
 		{
-			if ( (arr[i] + arr[j] == value) && (arr[i]!=arr[j]) && (i != j))
+			if ( arr[i] + arr[j] == value)
 				count++;
 		}
 	}
@@ -33,31 +12,57 @@ int countPairs1(int* arr, int len, int value)
 }
 int countPairs2(int* arr, int len, int value)
 {
-	int k = 0;
- for (int i = 0; i < len-1; i++)
- {
-   for (int j = len-1; j > i; j--)
-   {
-     if (arr[i] + arr[j] == value)
-     {
-       k++;
-
-     }
-   }
- }
- return k;
+	int count = 0;
+	int i = 0 , j = len - 1;
+	int k;
+	while (true) {
+		if (i == j)
+			return count;
+		else if (arr[i] + arr[j] < value)
+			i++;
+		else if (arr[i] + arr[j] > value)
+			j--;
+		else  if (arr[i] + arr[j] == value) {
+			count++;
+			for (k = i + 1; k < j; k++) {
+				if (arr[k] + arr[j] == value)
+					count++;
+			}
+			j = j - 1;
+		}
+                 } 
 }
 int countPairs3(int* arr, int len, int value)
 {
-	int key = 0, count = 0;
-	for (int i = 0; i < len; i++) {
-		key = value;
-		key = key - arr[i];
-		int x = cbinsearch(arr, len, key);
-		if (x >= 0 && x > i)
-			count++;
-		if (x < 0)
-			continue;
+	int count = 0;
+	for (int i = 0; i < len - 1; i++)
+	{
+		int left = i, right = len;
+		while (left < right - 1)
+		{
+			int mid = (left + right) / 2;
+			if (arr[i] + arr[mid] == value)
+			{
+				count++;
+				int j = mid + 1;
+				while (arr[i] + arr[j] == value && j < right)
+				{
+				count++;
+					j++;
+				}
+				j = mid - 1;
+				while (arr[i] + arr[j] == value && j > left)
+				{
+					count++;
+					j--;
+				}
+				break;
+			}
+			if (arr[i] + arr[mid] > value)
+				right = mid;
+			else
+				left = mid;
+		}
 	}
 	return count;
 } 
